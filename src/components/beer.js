2636c4200e0172bcf2ad145820ebc31c92d7cd0a
Ruben Arcos <ruben.arcos06@gmail.com>
2023-08-30 23:36:23 -0500

    added heart button and likes count

--- src/components/beer.js
+++ src/components/beer.js
@@ -1,15 +1,33 @@
-import React from 'react'
-import '../App.css'
+// import React from "react";
+import React, { useState } from "react";
+import "../App.css";
+// eslint-disable-next-line
+import Heart from "react-animated-heart";
 
-export default function beer(props) {
-    console.log(props)
-    // child component
+export default function Beer(props) {
+  console.log(props);
+
+  const [isLiked, setLiked] = useState(false);
+  const [likesCount, setLikesCount] = useState(0);
+
+  const toggleLike = () => {
+    setLiked((prevIsLiked) => !prevIsLiked);
+    setLikesCount((prevLikesCount) => (isLiked ? prevLikesCount - 1 : prevLikesCount + 1));
+  };
+
+
+  // child component
   return (
-    <li className='beerInfo'>
-        <img className='beerImg' src={props.beer.image_url}  alt={props.beer.name}/>
-        <h3 className='beerName'>{props.beer.name}</h3>
-        <p className='description'>{props.beer.description}</p>
+    <li className="beerInfo">
+      <img
+        className="beerImg"
+        src={props.beer.image_url}
+        alt={props.beer.name}
+      />
+      <h3 className="beerName">{props.beer.name}</h3>
+      <p className="description">{props.beer.description}</p>
+      <Heart isClick={isLiked} onClick={toggleLike} />
+      <p>Likes: {likesCount}</p>
     </li>
-  )
+  );
 }
-
